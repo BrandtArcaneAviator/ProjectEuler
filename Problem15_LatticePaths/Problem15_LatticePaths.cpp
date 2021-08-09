46c196575d72d47f5f68480033f882aaaab95db0
BrandtArcaneAviator <steven.brandt17@gmail.com>
2021-08-08 22:17:43 -0400

    Modified problem 757 .cpp to reduce duplicate counting issue; worked on code structure of problem 15 .cpp.

--- Problem15_LatticePaths/Problem15_LatticePaths.cpp
+++ Problem15_LatticePaths/Problem15_LatticePaths.cpp
@@ -1,113 +1,225 @@
-// Program to count the maximum number of routes through
-// a lattice grid of a by b cells (represented as a matrix
-// M(x,y)) starting at the top left corner (i.e. x=0, y=0)
-// and ending in the lower right corner (i.e. x=a, y=b).
-// Note that during this route we only allow moves that
-// go right one grid space (e.g. from x=0 to x=1) or go
-// down one grid space (e.g. from y=0 to y=1).
-// 
-// For reference, here is an example grid (2x2):
-	// 0,0 -- 1,0 -- 2,0
-	// ^      ^      ^
-	// ^      ^      ^
-	// 0,1 -- 1,1 -- 2,1
-	// ^      ^      ^
-	// ^      ^      ^
-	// 0,2 -- 1,2 -- 2,2
-//  
-// In this example grid, there are exactly 6 routes:
-	// 1) 0,0 -> 1,0 -> 2,0 -> 2,1 -> 2,2.
-	// 2) 0,0 -> 1,0 -> 1,1 -> 2,1 -> 2,2.
-	// 3) 0,0 -> 1,0 -> 1,1 -> 1,2 -> 2,2.
-	// 4) 0,0 -> 0,1 -> 1,1 -> 2,1 -> 2,2.
-	// 5) 0,0 -> 0,1 -> 1,1 -> 1,2 -> 2,2.
-	// 6) 0,0 -> 0,1 -> 0,2 -> 1,2 -> 2,2.
-//
-// Thus, for an arbitrary a by b grid, we need to implement
-// the following:
-	// i) A matrix representing the grid (a 2-d std::vector).
-	// ii) A "2-d" point (can use a class object for this),
-	//    that points to the current position on this grid
-	//    (maybe use an set of iterator that point to the
-	//    element in the matrix?).
-	// iii) Member functions which check the current position
-	//    and execute a specific path (right one, down one).
-	// iv) A counter for the number of possible routes.  
-// Note that we only care about unique routes, so this
-// should only ever increase when we take unique paths.
-// To this end, we can record previous paths (e.g. using
-// push_back of sets of two values into a vector; a 2-d point
-// class would be highly valuable for this) in another
-// set of vectors (or a matrix) and compare them to
-// new paths to determine if we increase this counter.
-// Notably, we could alternatively implement this inside 
-// the pathing itself, such that if we've already approached 
-// in a certain way we can avoid that path in the future
-// (one example here is that path 1) for the 2x2 above
-// goes through the point 2,0 and only this path goes
-// through this point). Furthermore, we can immediately
-// identify a few known routes (and thus avoid simulating
-// these):
-	// 1) Go directly right to end of cells (i.e. x=a),
-	//    then go directly down to end of cells (i.e., y=b).
-	// 2) Go directly down to end of cells (i.e. y=b),
-	//    then go directly right to end of cells (i.e., x=a).
-//  
-// There are two ways we can explore paths, a) choosing
-// moves randomly (i.e. using an RNG with equal values
-// between move right, move down) and checking the final
-// path against previous paths for uniqueness (but this
-// may repeat paths often and needlessly), or b) systematically
-// by exploring all variations of potential paths.
-// I'll note that, e.g. for a 2x2 grid, a given path
-// can only EVER have two moves to the right and two moves
-// down, thus there are only a set number of permutations
-// of executing these two moves (which we know in this case
-// to be the six permutations above).  This is notably
-// a series of binary (off = move right, on = move down)
-// choices whose length must be fixed (again, we MUST have
-// 2 moves right and 2 moves down in the 2x2 grid).
-// Extending this to an a x b grid, we know that we MUST
-// have a moves right (the "off" choice) and b moves down
-// (the "on" choice), so we only need to find the binary
-// permutations of this sequence (again, we know this
-// sequence must have length a + b).  Systematically,
-// this mean we set up "vectors" that represent choices
-// of these permutations.  Using the 2x2 example above,
-// this would be like (using 0 and 1 for move right, move
-// down):
-	// vec1 = { 0, 0, 1, 1 }
-	// vec2 = { 0, 1, 0, 1 }
-	// vec3 = { 0, 1, 1, 0 }
-	// vec4 = { 1, 0, 0, 1 }
-	// vec5 = { 1, 0, 1, 0 }
-	// vec6 = { 1, 1, 0, 0 }
-// Note, in this way, we can fix the requirement that we only
-// have "b" moves down by the sum of all elements in the
-// vector (i.e. there must be "b" 1 values here), but also 
-// note this condition also fixes "a" moves right due to the
-// vector length being a + b.
-//  
-//
+/*
+Program to count the maximum number of routes through
+ a lattice grid of a by b cells (represented as a matrix
+ M(x,y)) starting at the top left corner (i.e. x=0, y=0)
+ and ending in the lower right corner (i.e. x=a, y=b).
+ Note that during this route we only allow moves that
+ go right one grid space (e.g. from x=0 to x=1) or go
+ down one grid space (e.g. from y=0 to y=1).
+ 
+ For reference, here is an example grid (2x2):
+	 0,0 -- 1,0 -- 2,0
+	 ^      ^      ^
+	 ^      ^      ^
+	 0,1 -- 1,1 -- 2,1
+	 ^      ^      ^
+	 ^      ^      ^
+	 0,2 -- 1,2 -- 2,2
+  
+ In this example grid, there are exactly 6 routes:
+	 1) 0,0 -> 1,0 -> 2,0 -> 2,1 -> 2,2.
+	 2) 0,0 -> 1,0 -> 1,1 -> 2,1 -> 2,2.
+	 3) 0,0 -> 1,0 -> 1,1 -> 1,2 -> 2,2.
+	 4) 0,0 -> 0,1 -> 1,1 -> 2,1 -> 2,2.
+	 5) 0,0 -> 0,1 -> 1,1 -> 1,2 -> 2,2.
+	 6) 0,0 -> 0,1 -> 0,2 -> 1,2 -> 2,2.
+
+ Thus, for an arbitrary a by b grid, we need to implement
+ the following:
+	 i) A matrix representing the grid (a 2-d std::vector).
+	 ii) A "2-d" point (can use a class object for this),
+	    that points to the current position on this grid
+	    (maybe use an set of iterator that point to the
+	    element in the matrix?).
+	 iii) Member functions which check the current position
+	    and execute a specific path (right one, down one).
+	 iv) A counter for the number of possible routes.  
+ Note that we only care about unique routes, so this
+ should only ever increase when we take unique paths.
+ To this end, we can record previous paths (e.g. using
+ push_back of sets of two values into a vector; a 2-d point
+ class would be highly valuable for this) in another
+ set of vectors (or a matrix) and compare them to
+ new paths to determine if we increase this counter.
+ Notably, we could alternatively implement this inside 
+ the pathing itself, such that if we've already approached 
+ in a certain way we can avoid that path in the future
+ (one example here is that path 1) for the 2x2 above
+ goes through the point 2,0 and only this path goes
+ through this point). Furthermore, we can immediately
+ identify a few known routes (and thus avoid simulating
+ these):
+	 1) Go directly right to end of cells (i.e. x=a),
+	    then go directly down to end of cells (i.e., y=b).
+	 2) Go directly down to end of cells (i.e. y=b),
+	    then go directly right to end of cells (i.e., x=a).
+  
+ There are two ways we can explore paths, a) choosing
+ moves randomly (i.e. using an RNG with equal values
+ between move right, move down) and checking the final
+ path against previous paths for uniqueness (but this
+ may repeat paths often and needlessly), or b) systematically
+ by exploring all variations of potential paths.
+ I'll note that, e.g. for a 2x2 grid, a given path
+ can only EVER have two moves to the right and two moves
+ down, thus there are only a set number of permutations
+ of executing these two moves (which we know in this case
+ to be the six permutations above).  This is notably
+ a series of binary (off = move right, on = move down)
+ choices whose length must be fixed (again, we MUST have
+ 2 moves right and 2 moves down in the 2x2 grid).
+ Extending this to an a x b grid, we know that we MUST
+ have a moves right (the "off" choice) and b moves down
+ (the "on" choice), so we only need to find the binary
+ permutations of this sequence (again, we know this
+ sequence must have length a + b).  Systematically,
+ this mean we set up "vectors" that represent choices
+ of these permutations.  Using the 2x2 example above,
+ this would be like (using 0 and 1 for move right, move
+ down):
+	 vec1 = { 0, 0, 1, 1 }
+	 vec2 = { 0, 1, 0, 1 }
+	 vec3 = { 0, 1, 1, 0 }
+	 vec4 = { 1, 0, 0, 1 }
+	 vec5 = { 1, 0, 1, 0 }
+	 vec6 = { 1, 1, 0, 0 }
+ Note, in this way, we can fix the requirement that we only
+ have "b" moves down by the sum of all elements in the
+ vector (i.e. there must be "b" 1 values here), but also 
+ note this condition also fixes "a" moves right due to the
+ vector length being a + b. For the 20x20 goal, this means
+ we have a 40 element vector which has 20 1-values
+ and 20 0-values and we find all the unique
+ permutations of this vector; note the number of
+ unique vectors equals the number of unique paths.
+ We can thus make a vector of these binary path
+ vectors using a vector of a class of these vectors.
+*/  
 
 
 #include <iostream>
+#include <numeric> // For std::accumulate.
 #include <vector>
 
+/* 
+Vector class for a given path (0=move right,
+1 = move down): 
+*/
+class BinaryPath
+{
+private:
+	std::vector<int> m_binarypath{};
+	int m_a{};
+	int m_b{};
 
-// 2-d Point Class:
+public:
+	// Default initalizes a 1 by 1 grid.
+	BinaryPath() : m_a{ 1 }, m_b{ 1 }
+	{
+		m_binarypath.reserve(2);
+	}
 
+	// For an axb grid:
+	BinaryPath(int& a, int& b) : m_a{ a }, m_b{ b }
+	{
+		m_binarypath.reserve((m_a + m_b));
+	}
 
+	std::vector<int>& getPath() { return m_binarypath; }
+	int& getPathStep(int& nElement1) { return m_binarypath.at(nElement1); }
 
-// 2x2 Grid Class:
+	/*
+	To set the binary value of a given path step.
+	Note nElement goes from 0 to a + b - 1,
+	and that nValue should only have 0 or 1 values.
+	*/
+	void setPathStep(int& nElement, int nValue)
+	{
+		m_binarypath.at(nElement) = nValue;
+	}
 
+	/*
+	Recall a 1 value is a move down. This then
+	returns true if the sum of values in the vector
+	equals b, i.e. there are b moves down in the path.
+	Note again this also means there are a moves right
+	since the vector length is a + b and moves right are
+	0 values.  This is used to check that the vector is
+	a valid permutation, but does not guarentee uniquenss.
+	*/
+	bool checkNumMoves()
+	{
+		if ((std::accumulate(m_binarypath.begin(), m_binarypath.end(), 0)) == m_b)
+			return true;
+		else
+			return false;
+	}
 
-
+};
 
 int main()
 {
-	// 
+	/*
+	Ask user to input the size of grid a x b:
+	*/
+	std::cout << "Enter the width (max x) of lattice grid: ";
+	int a{};
+	std::cin >> a;
+	std::cout << "Enter the height (max y) of lattice grid: ";
+	int b{};
+	std::cin >> b;
 
+	/*
+	Make a vector of BinaryPath vectors which needs
+	dynamic length (we don't know the number of
+	unique permutations yet).
+	*/
+	std::vector<BinaryPath> uniquePaths{};
+
+	/*
+	Initialize a binarypath object to manipulate:
+	*/
+	BinaryPath currentPath{ a, b };
+
+	/*
+	Set up an initial path, which does b moves down first,
+	and then a moves right:
+	*/
+	for (int i{ 0 }; i < (a + b); ++i)
+	{
+		// If there are b moves down:
+		if (currentPath.checkNumMoves())
+		{
+			// Add move right.
+			currentPath.setPathStep(i, 0);
+		}
+		else
+		{
+			// Add move down.
+			currentPath.setPathStep(i, 1);
+		}
+	}
+	// Now we push_back this as the first unique vector:
+	uniquePaths.push_back( currentPath );
+
+	/*
+	Now we need to run through permutations of paths
+	which satisfy checkNumMoves() and find unique ones.
+	Note again that each element can only have either 0
+	or 1 values.
+	*/
+	
+	// Outer loop for permutations:
+	/*
+	for ()
+	{
+		// Inner loop for constructing currentPaths:
+		for ()
+		{
+
+		}
+	}
+	*/
 
 
 	return 0;
--- Problem757_StealthNum/Problem757_StealthNum.cpp
+++ Problem757_StealthNum/Problem757_StealthNum.cpp
@@ -1,3 +1,9 @@
+// Currently yields incorrect values for StealthNum,
+// likely due to recounting unique sets of a,b,c,d.
+// This is slightly remedied by the use of additional
+// if statements, but the code still doesn't seem
+// to give correct values and runs rather slowly.
+// 
 // Program to determine the number of "stealthy" numbers,
 // denoted the counter stealthNum, in the range 
 // 1 <_ N <_ N_max for positive integer N as defined by 
@@ -11,26 +17,20 @@
 // a,b,c,d (notably these all must be less than N by 
 // condition i)).  Also note that by associativity
 // of multiplication, we prove ab = cd by showing
-// ab = N and cd = N.  Note we must resolve the integer
-// division problem via static_casts to double and back
-// for these divisions.
+// ab = N and cd = N.
 // 
 // We then check condition ii) by checking a + b = c + d + 1,
-// (which notably also excludes the cases (a=c, b=d) and
-// (a=d, b=c) automatically).  If this condition is satisfied,
+// (which only excludes the cases (a=c, b=d) and
+// (a=d, b=c) if we also check that ab = cd, which double 
+// checks i)).  If this condition is satisfied,
 // then we have found a stealthy number, we increment
 // stealthNum and move to the next N value.  These loops
 // are repeated for all values of N in the given range.
 
-// This code is meant to demonstrate this basic algorithm for
-// small N values.  For larger N values (those of the problem
-// 757 posed by ProjectEuler, i.e. N_max = 10^14), see a
-// separate code in the same folder as this code.
-
 #include <cstdint>
 #include <iostream>
 
-using largeint_t = std::int_least32_t;
+using largeint_t = std::int_least64_t;
 
 // Class for bundling a, b, c, d, for given value of N.
 // Includes an extra boolean value that determines if the
@@ -53,8 +53,7 @@ public:
     }
 
     StealthNumSet(largeint_t& N, largeint_t& a, largeint_t& c)
-        : m_n{ N }, m_a{ a }, m_b{ static_cast<largeint_t>(N / static_cast<double>(a)) },
-        m_c{ c }, m_d{ static_cast<largeint_t>(N / static_cast<double>(c)) },
+        : m_n{ N }, m_a{ a }, m_b{ (N / a) }, m_c{ c }, m_d{ (N / c) },
         m_bool{ false }
     {
     }
@@ -70,12 +69,12 @@ public:
     void setab(largeint_t& a1)
     {
         m_a = a1;
-        m_b = static_cast<largeint_t>(m_n / static_cast<double>(a1));
+        m_b = (m_n / a1);
     }
     void setcd(largeint_t& c1)
     {
         m_c = c1;
-        m_d = static_cast<largeint_t>(m_n / static_cast<double>(c1));
+        m_d = (m_n / c1);
     }
 
     // Checks condition ii) for stealthy numbers.
@@ -83,18 +82,43 @@ public:
     // set functions and the second constructor, i.e.
     // they show ab = N and cd = N and thus ab = cd is true.
     // Also note the cases (a=c, b=d), (a=d,b=c) are
-    // automatically excluded by this if statement.
+    // automatically excluded by this if statement when
+    // we include ab = cd as a secondary condition.
+    // Duplicate combinations of a,b,c,d are a problem though!
+    // To avoid recounting, we need to force a <= b and c <= d
+    // (e.g. to avoid recounting N=4, we note the 1,4,2,2 and 
+    // 4,1,2,2 permutations are identical) such that a,b and 
+    // c,d don't overshare values.  We implement this as 
+    // an if statement inside the other. 
+    // 
+    // We also need to address duplication between sets of
+    // a,b and c,d (e.g. for N=4 above, 2,2,1,4 is a 
+    // duplicate of 1,4,2,2). Since a and c set b and d,
+    // we can avoid this by also forcing a <= c (in which
+    // case, only the 1,4,2,2 variant will be counted).
+    // We implement this as the condition cCounter <= aCounter
+    // in the for loop in main (this reduces # of calcs
+    // significantly).
     //
     // If this function returns true, then the loops over a,
     // c should be ended and we step to the next N.  While
-    // m_bool is false, 
+    // m_bool is false, both loops continue. 
     bool checkStealth()
     {
         // Checks condition ii) only, since i) is forced.
-        if ((m_a + m_b) == (m_c + m_d + 1))
+        if (((m_a + m_b) == (m_c + m_d + 1)) && ((m_a * m_b) == (m_c * m_d)))
+        {
+            // Avoids duplicate variations between a,b and 
+            // c,d values.
+            if ((m_a <= m_b) && (m_c <= m_d))
+            {
             m_bool = true; // Implies N is stealthy number.
+            }
+        }
         else
+        {
             m_bool = false;
+        }
 
         return m_bool;
     }
@@ -107,7 +131,7 @@ public:
 int main()
 {
     // Request the maximum upper bound for N from user:
-    std::cout << "Enter the maximum value of N (positive integer: "
+    std::cout << "Enter the maximum value of N (positive integer): "
         << '\n';
     largeint_t nMax{};
     std::cin >> nMax;
@@ -128,20 +152,21 @@ int main()
     {
         numSetN.setN(nCounter);
 
-        // Loop over a, from 1 to the current N:
-        for (largeint_t aCounter{ 1 }; aCounter <= nCounter; ++aCounter)
+        // Loop over c, from 1 to the current N:
+        for (largeint_t cCounter{ 1 }; cCounter <= nCounter; ++cCounter)
         {
-            numSetN.setab(aCounter); // Sets a,b, in the object
+            numSetN.setcd(cCounter); // Sets a,b, in the object
 
-            // Loop over c, from 1 to the current N:
-            for (largeint_t cCounter{ 1 }; cCounter <= nCounter; ++cCounter)
+            // Loop over a, from 1 to the current a.
+            // Uses cCounter to avoid duplicate sets of a,b,c,d.
+            for (largeint_t aCounter{ 1 }; aCounter <= cCounter; ++aCounter)
             {
-                numSetN.setcd(cCounter); // Sets c,d in the object
+                numSetN.setab(aCounter); // Sets c,d in the object
 
                 // If this number set is stealthy:
                 if (numSetN.checkStealth())
                 {
-                    ++stealthNum; // increment stealthNum,  Duplicate numbers throwing this off!
+                    ++stealthNum; // increment stealthNum
                     break;
                 }
             }
